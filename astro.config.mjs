098939e504a80c45facc76922a8018d9ec814266
jordan <sudo.sendmail.jordan@gmail.com>
2025-10-27 16:17:38 -0700

    refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import mdx from "@astrojs/mdx";
 export default defineConfig({
   integrations: [expressiveCode({
       themes: ['catppuccin-macchiato', 'catppuccin-latte'],
+    //   themeCssSelector: (theme) => `[data-theme='${theme.name}']`,
     // plugins: [pluginLineNumbers()],
   }), mdx()],
   vite: {
--- src/data.js
+++ src/data.js
@@ -1,91 +1,91 @@
 export var programData = [
     {
         id: "kde",
-        icon: "/src/assets/kde.svg",
+        icon: "kde.svg",
         link: "programs/kde.html",
         fileArray: ["CONTRIBUTING.md",],
         description: "KDE Plasma is a graphical shell developed by the KDE community for Unix-like operating systems."
     },    
     {
         id: "gnome",
-        icon: "/src/assets/gnome.svg",
+        icon: "gnome.svg",
         link: "programs/gnome.html",
         fileArray: ["CONTRIBUTING.md"],
         description: "GNOME is a free and open-source desktop environment for Linux and other Unix-like operating systems."
     },
     {
         id: "hyprland",
-        icon: "/src/assets/hyprland.svg",
+        icon: "hyprland.svg",
         link: "programs/hyprland.html",
         fileArray: ["CONTRIBUTING.md"],
         description: "Hyprland is an independent, highly customizable, dynamic tiling Wayland compositor that doesn't sacrifice on its looks."
     },
     {
         id: "xfce",  
-        icon: "/src/assets/xfce.svg",
+        icon: "xfce.svg",
         link: "programs/xfce.html", 
         fileArray: ["CONTRIBUTING.md"],
         description: "Xfce is a lightweight desktop environment for UNIX-like operating systems. It aims to be fast and low on system resources, while still being visually appealing and user friendly.", 
     },
     // { 
     //     id: "waybar",
-    //     icon: "/src/assets/shell.svg",
+    //     icon: "shell.svg",
     //     link: "programs/waybar.html",
     //     fileArray: ["CONTRIBUTING.md"],
     //     description: "empty"
     // },
     {
         id: "micro",
-        icon: "/src/assets/micro.svg",
+        icon: "micro.svg",
         link: "programs/micro.html",
         fileArray: [".config/micro/settings.json"],
         description: "Micro is a modern and intuitive text editor that runs in your terminal. It has features like multiple cursors, syntax highlighting, colorschemes, plugins, and a terminal emulator."
     },
     {
         id: "neovim",
-        icon: "/src/assets/neovim.svg",
+        icon: "neovim.svg",
         link: "programs/neovim.html",
         fileArray: ["CONTRIBUTING.md"],
         description: "Neovim is a modern, fast and feature-rich editor that is fully compatible with Vim. It supports plugins, GUIs, LSP, Lua, and more."
     },
     {
         id: "vscode",
-        icon: "/src/assets/vscode.svg",
+        icon: "vscode.svg",
         link: "programs/vscode.html",
         fileArray: [".config/Code/User/settings.json", ".config/Code/User/keybindings.json"],
         description: "Visual Studio Code is a source-code editor developed by Microsoft for Windows, Linux, macOS and web browsers."
     },
     { 
         id: "bash",
-        icon: "/src/assets/bash.svg",
+        icon: "bash.svg",
         link: "programs/bash.html",
         fileArray: [".bashrc"],
         description: "Bash is a free software shell that supports command-line editing, job control, shell functions, arrays, and more. It is compatible with sh and conforms to the POSIX standard."
     },
     { 
         id: "eza",
-        icon: "/src/assets/shell.svg",
+        icon: "shell.svg",
         link: "programs/eza.html",
         fileArray: [".config/eza/theme.yml"],
         description: "eza is a modern alternative for the venerable file-listing command-line program ls that ships with Unix and Linux operating systems, giving it more features and better defaults."
     },
     {
         id: "ghostty",
-        icon: "/src/assets/ghostty.png",
+        icon: "ghostty.png",
         link: "programs/ghostty.html",
         fileArray: [".config/ghostty/config"],
         description: "Ghostty is a fast, feature-rich, and cross-platform terminal emulator that uses platform-native UI and GPU acceleration."
     },
     { 
         id: "kitty",
-        icon: "/src/assets/shell.svg",
+        icon: "shell.svg",
         link: "programs/kitty.html",
         fileArray: ["CONTRIBUTING.md"],
         description: "kitty is a fast, feature-rich, and cross-platform terminal emulator that uses GPU and SIMD vector CPU instructions for best performance. It supports graphics, ligatures, hyperlinks, scripting, kittens, and more."
     },
     { 
         id: "alacritty",
-        icon: "/src/assets/shell.svg",
+        icon: "shell.svg",
         link: "programs/alacritty.html",
         fileArray: ["CONTRIBUTING.md"],
         description: "Alacritty is a modern terminal emulator that integrates with other applications and allows for extensive configuration."
