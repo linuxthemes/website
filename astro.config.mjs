b88b93d4484afb1a9e5241b9f8f4553c5d8df1a9
jordan <sudo.sendmail.jordan@gmail.com>
2025-10-26 20:59:43 -0700

    sync

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-import expressiveCode from 'astro-expressive-code';
-
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
+
+// @ts-check
+import { defineConfig } from "astro/config";
+import expressiveCode from "astro-expressive-code";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   integrations: [expressiveCode()],
-
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
